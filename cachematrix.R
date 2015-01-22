897898fb262a8f6b04a8cb4a97de006d53203f45
Dale Schroyer <dales@promodel.com>
2015-01-22 14:52:35 -0500

    Generated functions for caching the inverse of a matrix for programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## cache the inverse of a matrix in the environment using the <<- operator
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setsetinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## If the inverse is cached retrieve it, if not generate it
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinverse(m)
+      m
 }
